a8f70111b0e63279e9c9f2cd3191e592ab0f0ca9
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2016-01-15 16:44:50 +0100

    fancy colored logs

--- index.js
+++ index.js
@@ -1,98 +1,114 @@
 
+var colors = require('colors');
+
 
 var mraa = require('mraa'); //require mraa
-console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the Intel XDK console
+console.log('---------------------------'.inverse);
+console.log('WELCOME TO SOCIAL GARDENING'.inverse);
+console.log('---------------------------'.inverse);
 
-const BACKEND_URL = 'http://192.168.2.2:3000';
-
-var clientio = require('socket.io-client')(BACKEND_URL);
-var client    = clientio.connect(BACKEND_URL);
+console.log('MRAA Version: ' + mraa.getVersion());
+var BACKEND_URL = 'http://192.168.2.4:3000';
 
 
-var
 
-client.on('connect', function(){
-
-    console.log('connected to backend');
-
-    client.on('backend:configuration', function(data) {
-
-    });
-});
-
-//var myOnboardLed = new mraa.Gpio(3, false, true); //LED hooked up to digital pin (or built in pin on Galileo Gen1)
-var myOnboardLed = new mraa.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Intel Galileo Gen2 as well as Intel Edison)
+// initialize OnBoard LED
+var myOnboardLed = new mraa.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Intel Galileo Gen2)
 myOnboardLed.dir(mraa.DIR_OUT); //set the gpio direction to output
 var ledState = true; //Boolean to hold the state of Led
 
-// inititialize sensors
-var soilSensorD = new mraa.Gpio(7);
-soilSensorD.dir(mraa.DIR_IN);
+// initialize sensors
 var waterSensorA = new mraa.Aio(0);
 var waterLevel = waterSensorA.read();
-var waterLevelNormalized = 0;
 var soilSensorA = new mraa.Aio(1);
 var moisture = soilSensorA.read();
-var moistureNormalized = 0;
+var readyToWater = true;
 
 //initialize actors
 var relayD = new mraa.Gpio(8);
 relayD.dir(mraa.DIR_OUT);
 
+// initialize vars for socket-io
+var clientio = require('socket.io-client')(BACKEND_URL);
+var client    = clientio.connect(BACKEND_URL);
 
+var configMoisture = 69;
+var configuration = {
+    moisture: 50
+};
+
+setUpSocket();
 periodicActivity(); //call the periodicActivity function
 
 function periodicActivity()
 {
-    //relayD.write(1);
-    myOnboardLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
-    ledState = !ledState; //invert the ledState
-
     readSensorValues();
     printSerial();
     checkToWater();
 
-    setTimeout(periodicActivity,1000); //milliseconds
+    setTimeout(periodicActivity,500); //milliseconds
 }
+
+
+function setUpSocket()
+{
+    client.on('connect', function(){
+
+        console.log('connected to backend'.green);
+
+        client.on('backend:configuration', function(data) {
+            console.log(data.moisture);
+            configuration.moisture = data.moisture;
+            configMoisture = data.moisture;
+            myOnboardLed.write(ledState?1:0);
+            ledState = !ledState; //invert the ledState
+        });
+    });
+}
+
 
 function checkToWater()
 {
-    if (moisture > 600 && waterLevel > 220) {
+    //moisture      trocken 0 -> 100 nass
+    //waterLevel    leer    0 -> 100 voll
+    if (moisture < configMoisture && waterLevel > 10 && readyToWater == true) {
+        readyToWater = false;
+        setTimeout(function () {
+            readyToWater = true;
+            console.log('TIMEOUT -> set watering flag true'.yellow);
+        }, 10000); //ms
+
         relayD.write(0);
-        console.log('moisture > 600 && waterLevel < 1000  -> ON');
-    } else {
-        relayD.write(1);
+        console.log('start watering -> relay ON'.yellow);
+        setTimeout(function () {
+            relayD.write(1);
+            console.log('TIMEOUT -> stop watering -> relay OFF'.yellow);
+        }, 3000);
+        
     }
 }
 
 function readSensorValues()
 {
-    readSoilMoisture();
-    readWaterLevel();
-    clientEmit()
-}
-
-function readSoilMoisture()
-{
-    moisture = soilSensorA.read();
-}
-
-function readWaterLevel()
-{
-    waterLevel = waterSensorA.read();
+    moisture = 100 - Math.round((soilSensorA.read() - 250) / 4.5);
+    if (moisture > 100) 
+        moisture = 100;
+    waterLevel = Math.round(waterSensorA.read() / 7);
+    if (waterLevel > 100)
+        waterLevel = 100;
+    clientEmit();
 }
 
 function clientEmit()
 {
-    waterLevelNormalized = Math.round(0.15 * waterLevel);
-    moistureNormalized = Math.round(1 * moisture);
-    client.emit('sensor:waterlevel', {value: waterLevelNormalized});
-    client.emit('sensor:moisture', {value: moistureNormalized});
-    console.log('round water = ' + waterLevelNormalized);
+    client.emit('sensor:waterlevel', {value: waterLevel});
+    client.emit('sensor:moisture', {value: moisture});
 }
 
 function printSerial()
 {
-    console.log('moisture = ' + moisture);
-    console.log('water level = ' + waterLevel);
+    console.log(colors.cyan('water level =  %s'), waterLevel);
+    console.log(colors.cyan('moisture = ' + moisture));
+    console.log('configMoisture = ' + configMoisture);
+    console.log('');
 }
