00592fb70e21de3f036080dacb7bd347b28ad117
Simon Braun <sibraun@htwg-konstanz.de>
2016-01-15 15:55:56 +0100

    cleanup unused stuff

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 
 
 var mraa = require('mraa'); //require mraa
-console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the Intel XDK console
-
+console.log('MRAA Version: ' + mraa.getVersion());
 var BACKEND_URL = 'http://192.168.2.4:3000';
 
 var clientio = require('socket.io-client')(BACKEND_URL);
 var client    = clientio.connect(BACKEND_URL);
 
-var configMoisture = 555;
+var configMoisture = 69;
 var configuration = {
     moisture: 50
 };
@@ -21,8 +20,6 @@ client.on('connect', function(){
         console.log('               ' + data.moisture);
         configuration.moisture = data.moisture;
         configMoisture = data.moisture;
-        // !!old!!  configMoisture = ((100 - data.moisture) * 4) + 300;
-        //moisture - Feuchtigkeitswert 0-100
         myOnboardLed.write(ledState?1:0);
         ledState = !ledState; //invert the ledState
     });
@@ -30,19 +27,15 @@ client.on('connect', function(){
 });
 
 // initialize OnBoard LED
-var myOnboardLed = new mraa.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Intel Galileo Gen2 as well as Intel Edison)
+var myOnboardLed = new mraa.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Intel Galileo Gen2)
 myOnboardLed.dir(mraa.DIR_OUT); //set the gpio direction to output
 var ledState = true; //Boolean to hold the state of Led
 
 // initialize sensors
-var soilSensorD = new mraa.Gpio(7);
-soilSensorD.dir(mraa.DIR_IN);
 var waterSensorA = new mraa.Aio(0);
 var waterLevel = waterSensorA.read();
-var waterLevelNormalized = 0;
 var soilSensorA = new mraa.Aio(1);
 var moisture = soilSensorA.read();
-var moistureNormalized = 0;
 
 //initialize actors
 var relayD = new mraa.Gpio(8);
@@ -75,16 +68,16 @@ function checkToWater()
 function readSensorValues()
 {
     moisture = 100 - Math.round((soilSensorA.read() - 250) / 4.5);
+    if (moisture > 100) 
+        moisture = 100;
     waterLevel = Math.round(waterSensorA.read() / 7);
+    if (waterLevel > 100)
+        waterLevel = 100;
     clientEmit();
 }
 
 function clientEmit()
 {
-    //waterLevelNormalized = Math.round(0.15 * waterLevel);
-    //moistureNormalized = Math.round(1 * moisture);
-    //client.emit('sensor:waterlevel', {value: waterLevelNormalized});
-    //client.emit('sensor:moisture', {value: moistureNormalized});
     client.emit('sensor:waterlevel', {value: waterLevel});
     client.emit('sensor:moisture', {value: moisture});
 }
